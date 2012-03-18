334e647fbf88665f1ad2acb5803ced84d8ee8fe5
Michael C. Harris <michael@twofishcreative.com>
2012-03-18 05:06:23 +0000

    Add diigo.

---
+++ bookmarklets/bookmark_on_diigo.js
@@ -0,0 +1,1 @@
+javascript:(function()%7Bs=document.createElement(%22script%22);s.type=%22text/javascript%22;s.src=%22http://www.diigo.com/javascripts/webtoolbar/diigolet_b_h_b.js%22;document.body.appendChild(s);%7D)();
